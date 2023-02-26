eb4ed098f6e8d1688e1c4f1f59d2a00bf72480f3
AqibAurangzeb <aqibaurangzebdeveloper@gmail.com>
2023-02-26 04:56:36 +0000

    persist dark mode usage to local storage

--- index.js
+++ index.js
@@ -7,23 +7,37 @@
 // const cardBottom = document.getElementById("card-bottom");
 const lightDarkModeBtn = document.getElementById("light-dark-mode-btn");
 
+const mode = localStorage.getItem("darkMode");
+
+if (mode === "enabled") {
+  setColorsForDarkMode();
+}
+
 lightDarkModeBtn.addEventListener("click", function() {
   if (lightDarkModeBtn.textContent === "☀️") {
-    lightDarkModeBtn.textContent = "🌙";
-
-    document.documentElement.style.setProperty('--card-top-bg-color', '#6943FF');
-    document.documentElement.style.setProperty('--card-bottom-bg-color', '#1F2937');
-    document.documentElement.style.setProperty('--card-bottom-converted-card-bg-color', '#273549');
-    document.documentElement.style.setProperty('--card-bottom-converted-card-title-color', '#CCC1FF');
-    document.documentElement.style.setProperty('--card-bottom-converted-card-text-color', '#FFFFFF');
+    setColorsForDarkMode();
+    localStorage.setItem("darkMode", "enabled");
   }
   else {
-    lightDarkModeBtn.textContent = "☀️"; 
-
-    document.documentElement.style.setProperty('--card-top-bg-color', '#6943FF');
-    document.documentElement.style.setProperty('--card-bottom-bg-color', '#F4F4F4');
-    document.documentElement.style.setProperty('--card-bottom-converted-card-bg-color', '#FFFFFF');
-    document.documentElement.style.setProperty('--card-bottom-converted-card-title-color', '#5A537B');
-    document.documentElement.style.setProperty('--card-bottom-converted-card-text-color', '#353535'); 
+    setColorsForLightMode();
+    localStorage.setItem("darkMode", "disabled");
   }
 })
+
+function setColorsForDarkMode() {
+  lightDarkModeBtn.textContent = "🌙";
+  document.documentElement.style.setProperty('--card-top-bg-color', '#6943FF');
+  document.documentElement.style.setProperty('--card-bottom-bg-color', '#1F2937');
+  document.documentElement.style.setProperty('--card-bottom-converted-card-bg-color', '#273549');
+  document.documentElement.style.setProperty('--card-bottom-converted-card-title-color', '#CCC1FF');
+  document.documentElement.style.setProperty('--card-bottom-converted-card-text-color', '#FFFFFF');
+}
+
+function setColorsForLightMode() {
+  lightDarkModeBtn.textContent = "☀️"
+  document.documentElement.style.setProperty('--card-top-bg-color', '#6943FF');
+  document.documentElement.style.setProperty('--card-bottom-bg-color', '#F4F4F4');
+  document.documentElement.style.setProperty('--card-bottom-converted-card-bg-color', '#FFFFFF');
+  document.documentElement.style.setProperty('--card-bottom-converted-card-title-color', '#5A537B');
+  document.documentElement.style.setProperty('--card-bottom-converted-card-text-color', '#353535'); 
+}
